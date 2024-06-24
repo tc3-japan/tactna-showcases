b4ea0816db6d8375a10b1621ee3b7e298d7dc2b6
Tetsuo Shioda <shioda@tc3.co.jp>
2024-06-24 18:45:26 +0900

    Set default redirect uri

--- oidc-client-app/src/config.tsx
+++ oidc-client-app/src/config.tsx
@@ -3,8 +3,8 @@ import { assert } from './utils'
 
 const authority = assert(import.meta.env.VITE_OIDC_AUTHORITY);
 const clientId = assert(import.meta.env.VITE_OIDC_CLIENT_ID);
-const redirectUri = assert(import.meta.env.VITE_OIDC_REDIRECT_SIGN_IN);
-const postLogoutRedirectUri = assert(import.meta.env.VITE_OIDC_REDIRECT_SIGN_OUT);
+const redirectUri = import.meta.env.VITE_OIDC_REDIRECT_SIGN_IN || window.location.origin;
+const postLogoutRedirectUri = import.meta.env.VITE_OIDC_REDIRECT_SIGN_OUT || window.location.origin;;
 const signupEndpoint = import.meta.env.VITE_SIGNUP_ENDPOINT;
 const postSignupRedirectUri = import.meta.env.VITE_REDIRECT_SIGN_UP;
 const resourceServerUri = assert(import.meta.env.VITE_RESOURCE_SERVER_URI);
