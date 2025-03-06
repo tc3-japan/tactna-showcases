561b70611ed309fe8b4ffab8520d83b2d9eef5f1
sndyuk <sanada@sndyuk.com>
2025-03-06 14:33:50 +0900

    Pass team_id on login

--- oidc-client-app/src/config.tsx
+++ oidc-client-app/src/config.tsx
@@ -12,13 +12,18 @@ const postSignupRedirectUri = import.meta.env.VITE_REDIRECT_SIGN_UP;
 const resourceServerUri = assert(import.meta.env.VITE_RESOURCE_SERVER_URI);
 const audience = import.meta.env.VITE_AUDIENCE;
 
+const currUrl = new URL(window.location.href);
+
 export const oidcConfig: AuthProviderProps = {
   authority,
   client_id: clientId,
   redirect_uri: redirectUri,
   post_logout_redirect_uri: postLogoutRedirectUri,
   scope: 'openid',
-  extraTokenParams: { audience },
+  extraQueryParams: {
+    audience,
+    team_id: currUrl.searchParams.get("team_id") || "",
+  },
   onSigninCallback: (user: User | void): void => {
     const redirectUri = user?.state as string | undefined
     if (redirectUri && redirectUri.match("http(s)://.*")) {
