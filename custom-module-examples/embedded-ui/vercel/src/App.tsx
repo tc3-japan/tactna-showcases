4b0c6e8f0c303a1ce784105d9af14ab33a45cbf0
iwakrur <iwakura@tc3.co.jp>
2025-04-20 01:26:36 +0900

    chore
    Signed-off-by: iwakrur <iwakura@tc3.co.jp>

--- custom-module-examples/embedded-ui/vercel/src/App.tsx
+++ custom-module-examples/embedded-ui/vercel/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const pageName = (new URLSearchParams(window.location.search)).get('pageName');
+  const pageName = (new URLSearchParams(window.location.search)).get('pageName')?.match(/^[a-zA-Z]*/)?.[0];
 
   return (
     <>
