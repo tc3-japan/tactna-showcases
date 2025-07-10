efadf5af7117f7480b83e73d35367e41d402d44c
Tetsuo Shioda <shioda@tc3.co.jp>
2025-07-10 16:11:01 +0900

    fix: update postSignupRedirectUri fallback and add email scope

--- oidc-client-app/src/config.tsx
+++ oidc-client-app/src/config.tsx
@@ -8,7 +8,7 @@ const clientId = assert(import.meta.env.VITE_OIDC_CLIENT_ID);
 const redirectUri = import.meta.env.VITE_OIDC_REDIRECT_SIGN_IN || window.location.origin;
 const postLogoutRedirectUri = import.meta.env.VITE_OIDC_REDIRECT_SIGN_OUT || window.location.origin;
 const signupEndpoint = import.meta.env.VITE_SIGNUP_ENDPOINT;
-const postSignupRedirectUri = import.meta.env.VITE_REDIRECT_SIGN_UP;
+const postSignupRedirectUri = import.meta.env.VITE_REDIRECT_SIGN_UP || window.location.origin;
 const resourceServerUri = assert(import.meta.env.VITE_RESOURCE_SERVER_URI);
 const audience = import.meta.env.VITE_AUDIENCE;
 
@@ -19,7 +19,7 @@ export const oidcConfig: AuthProviderProps = {
   client_id: clientId,
   redirect_uri: redirectUri,
   post_logout_redirect_uri: postLogoutRedirectUri,
-  scope: 'openid',
+  scope: 'openid email',
   extraQueryParams: {
     audience,
     team_id: currUrl.searchParams.get("team_id") || "",
