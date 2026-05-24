8d426537ab7f9ea7d24341a94e855afc56186c01
iwakrur <iwakura@tc3.co.jp>
2026-05-24 13:44:13 +0900

    chore
    Signed-off-by: iwakrur <iwakura@tc3.co.jp>

--- oidc-client-app/src/views/Home.tsx
+++ oidc-client-app/src/views/Home.tsx
@@ -8,7 +8,7 @@ export const Home = () => {
     <Stack alignItems="center" spacing={2}>
       <img src="/react.svg" alt="React logo" height={80} />
       <Typography variant="h5">{currentConfigName ?? appConfig.appName}</Typography>
-      <p style={{ display: 'none' }}>ver. 20260501_1800</p>
+      <p style={{ display: 'none' }}>ver. 20260524_1400</p>
     </Stack>
   );
 };
